1ec05b452eeb030e3063572d7c9665eb466a899d
Ramireddy Reddy <RamireddyR@emids.com>
2022-04-07 12:32:47 +0530

    removed unwanted files

--- src/main/java/com/example/learning/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.learning.repository;
-
-import com.example.learning.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}

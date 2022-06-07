c0055aa668df65a615a7b35c089238a2c1b09bca
Ramireddy Reddy <RamireddyR@emids.com>
2022-06-07 06:51:46 +0530

    refactor 1

--- src/main/java/com/example/learning/service/AlphanumericSortComparatorTest.java
+++ src/main/java/com/example/learning/service/AlphanumericSortComparatorTest.java
@@ -26,10 +26,6 @@ public final class AlphanumericSortComparatorTest<T> implements  Comparator<T> {
         int ia = 0;
         int ib = 0;
 
-        // The longest run of digits wins.  That aside, the greatest
-        // value wins, but we can't know that it will until we've scanned
-        // both numbers to know that they have the same magnitude, so we
-        // remember it in BIAS.
         for (;; ia++, ib++) {
             char ca = charAt(a, ia);
             char cb = charAt(b, ib);
@@ -57,73 +53,44 @@ public final class AlphanumericSortComparatorTest<T> implements  Comparator<T> {
         String a = o1.toString();
         String b = o2.toString();
 
-        int ia = 0, ib = 0;
-        int nza = 0, nzb = 0;
+        int ia = 0;
+        int ib = 0;
+        int nza = 0;
+        int nzb = 0;
         char ca, cb;
         int result;
 
         while (true) {
-            // only count the number of zeroes leading the last number compared
-            nza = nzb = 0;
-
+            nza = 0;
+            nzb = 0;
             ca = charAt(a, ia);
             cb = charAt(b, ib);
-
-            // skip over leading zeros
             while (ca == '0') {
-                if (ca == '0') {
-                    nza++;
-                } else {
-                    // only count consecutive zeroes
-                    nza = 0;
-                }
-
-                // if the next character isn't a digit, then we've had a run
-                // of only zeros
-                // we still need to treat this as a 0 for comparison purposes
+                nza++;
                 if (!Character.isDigit(charAt(a, ia+1)))
                     break;
-
                 ca = charAt(a, ++ia);
             }
-
             while (cb == '0') {
-                if (cb == '0') {
-                    nzb++;
-                } else {
-                    // only count consecutive zeroes
-                    nzb = 0;
-                }
-
-                // if the next character isn't a digit, then we've had a run
-                // of only zeros
-                // we still need to treat this as a 0 for comparison purposes
+                nzb++;
                 if (!Character.isDigit(charAt(b, ib+1)))
                     break;
-
                 cb = charAt(b, ++ib);
             }
-
-            // process run of digits
             if (Character.isDigit(ca) && Character.isDigit(cb)) {
                 if ((result = compareRight(a.substring(ia), b
                         .substring(ib))) != 0) {
                     return result;
                 }
             }
-
             if (ca == 0 && cb == 0) {
-                // The strings compare the same.  Perhaps the caller
-                // will want to call strcmp to break the tie.
                 return nza - nzb;
             }
-
             if (ca < cb) {
                 return -1;
             } else if (ca > cb) {
                 return +1;
             }
-
             ++ia;
             ++ib;
         }
